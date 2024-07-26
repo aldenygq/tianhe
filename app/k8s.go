c671ecc1cbbb7673f8cd026fb66627086b2a5976
Alden <ygq13572549874@163.com>
2024-07-26 19:26:08 +0800

    Update k8s.go
    Signed-off-by: Alden <ygq13572549874@163.com>

--- app/k8s.go
+++ app/k8s.go
@@ -26,6 +26,24 @@ func ClusterEvent(c *gin.Context) {
 	ctx.Response(middleware.HTTP_SUCCESS_CODE, msg, data)
 	return
 }
+func ClusterUserList(c *gin.Context) {
+	ctx := middleware.Context{Ctx: c}
+	var param models.ParamClusterId
+	err := ctx.Validate(&param)
+	if err != nil {
+		middleware.LogErr(ctx.Ctx).Errorf(fmt.Sprintf("request param invalid:%v",err))
+		return
+	}
+	data,msg,err := service.ClusterUserList(ctx.Ctx,param)
+	if err != nil {
+		middleware.LogErr(ctx.Ctx).Errorf(fmt.Sprintf("%v",err))
+		ctx.Response(middleware.HTTP_FAIL_CODE, msg, "")
+		return
+	}
+
+	ctx.Response(middleware.HTTP_SUCCESS_CODE, msg, data)
+	return
+}
 func DeleteResource(c *gin.Context) {
 	ctx := middleware.Context{Ctx: c}
 	var param models.ParamReourceYaml
